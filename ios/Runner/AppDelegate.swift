5ad1bd0b23db7bdd904cd46beab19135491ef655
jungjaewoo2 <kdjjw@zetta-system.co.kr>
2025-11-10 18:49:16 +0900

    ios 1.0.0+3 다시
    아이콘 생성등(화질때문에)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,22 +10,9 @@ import UIKit
     GeneratedPluginRegistrant.register(with: self)
     
     // 상태바 스타일 설정 (주황색 배경에 맞춰 밝은 아이콘)
-    // Info.plist에서 UIViewControllerBasedStatusBarAppearance를 false로 설정했으므로
-    // 여기서 전역적으로 설정 가능
-    if #available(iOS 13.0, *) {
-      // iOS 13+ 에서는 SceneDelegate를 통해 설정하거나
-      // UIViewControllerBasedStatusBarAppearance가 false면 전역 설정 가능
-      // Info.plist에 UIStatusBarStyle을 설정했으므로 자동 적용됨
-    } else {
-      // iOS 12 이하 지원
-      UIApplication.shared.statusBarStyle = .lightContent
-    }
+    // Info.plist에서 UIViewControllerBasedStatusBarAppearance를 false로 설정하고
+    // UIStatusBarStyle을 UIStatusBarStyleLightContent로 설정했으므로 자동 적용됨
     
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-  
-  // iOS 9 이하 지원
-  override var preferredStatusBarStyle: UIStatusBarStyle {
-    return .lightContent
   }
 }
